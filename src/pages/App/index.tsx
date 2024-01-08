a917fddc07455d73d5fee3b0931b44bc4fd3c45b
joao paulo gonçalves de sena <jp.provenet@gmail.com>
2024-01-08 18:59:46 -0300

    add base project

---
+++ src/pages/App/index.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import { Container } from "./styles";
+
+export function App() {
+  return (
+    <Container>
+      <header>Header</header>
+
+      <main>
+        <Outlet />
+      </main>
+
+      <footer>footer</footer>
+    </Container>
+  );
+}
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,19 @@
+import { PokemonCard } from "../../components/PokemonCard";
+import { useQueryPokemonPage } from "../../hooks/useQueryPokemonPage";
+import { Container } from "./style";
+
+export function Home() {
+  const { data } = useQueryPokemonPage();
+  console.log(data);
+  return (
+    <Container>
+      <h1>{"Bem vindo(a) à Pokédex do Reprograma Jucás"}</h1>
+
+      <div className="gridCards">
+        {data?.map((pokemon) => {
+          return <PokemonCard pokemon={pokemon} />;
+        })}
+      </div>
+    </Container>
+  );
+}
